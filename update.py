1a397bd457368947ece803da3f693a1bb8ddf78d
JellyWX <judewrs@gmail.com>
2017-03-30 16:23:40 +0100

    updates for the installer to make it update itself

--- update.py
+++ update.py
@@ -5,10 +5,12 @@ from urllib.request import urlopen
 clean_install = True
 binary_version = 0
 binary_update = True
+installer_version = 0
+installer_update = True
 asset_version = 0
 asset_update = True
 
-asset_list = ['0','1','2','3','4','5','6','7','8','mark','mine','background','tile','overlay','overlay_2']
+asset_list = None
 
 def download(url,filename):
   r = urlopen(url)
@@ -38,9 +40,11 @@ except FileExistsError:
 
 try:
   with open('.meta','r') as f:
-    version_list_old = f.read().split(',')
+    version_list_old = f.read().split(';')
     binary_version = int(version_list_old[0])
-    asset_version = int(version_list_old[1])
+    installer_version = int(version_list_old[1])
+    asset_list_existing = version_list_old[2].split(',')
+    asset_version = int(version_list[3])
   clean_install = False
 except FileNotFoundError:
   print('No meta files found. This version is old or non-existant. Will perform full install')
@@ -54,11 +58,16 @@ if not clean_install:
     version_list = f.read().split(',')
     if int(version_list[0]) == binary_version:
       binary_update = False
-    if int(version_list[1]) == asset_version:
+    if int(version_list[1]) == installer_version:
+      installer_update = False
+    if int(version_list[3]) == asset_version:
       asset_update = False
+    asset_list = version_list[2].split(',')
 
-if clean_install or asset_update:
-  for item in asset_list:
+for item in asset_list:
+  if item in asset_list_existing and not asset_update:
+    continue
+  else:
     download('https://raw.githubusercontent.com/JellyWX/Minesweeper/master/assets/images/' + item + '.png','assets/images/' + item + '.png')
 
 if clean_install or binary_update:
@@ -66,3 +75,9 @@ if clean_install or binary_update:
     download('https://raw.githubusercontent.com/JellyWX/Minesweeper/master/main','main')
   else:
     download('https://raw.githubusercontent.com/JellyWX/Minesweeper/master/main.exe','main.exe')
+
+if clean_install or installer_update:
+  if plat == 0:
+    download('https://raw.githubusercontent.com/JellyWX/Minesweeper/master/installer','installer')
+  else:
+    download('https://raw.githubusercontent.com/JellyWX/Minesweeper/master/installer.exe','installer.exe')
